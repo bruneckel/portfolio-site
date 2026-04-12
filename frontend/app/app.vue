0e78e49c7971a1955fc84e2a1bb47e0539bd590e
Bruno Neckel <bruno@bnkcodes.com>
2026-04-12 17:19:06 -0300

    fix: set defaultLocale for i18n configuration

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="block">
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,13 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
   modules: [
-    '@nuxt/image',
-    '@nuxtjs/color-mode',
-    '@nuxtjs/device',
-    '@nuxtjs/i18n',
-    '@nuxtjs/tailwindcss'
-  ]
-})
+    "@nuxt/image",
+    "@nuxtjs/color-mode",
+    "@nuxtjs/device",
+    "@nuxtjs/i18n",
+    "@nuxtjs/tailwindcss",
+  ],
+
+  i18n: {
+    defaultLocale: "en",
+  },
+});
