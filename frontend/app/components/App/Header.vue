75da0a51b02e1ded9e6c9a5fe3a80b568041b2c5
Bruno Neckel <neckel.bw@gmail.com>
2026-06-17 23:47:14 -0300

    feat: add Tabs  component and improve Header

--- frontend/app/components/App/Header.vue
+++ frontend/app/components/App/Header.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { motion } from "motion-v";
+
 import whiteIsotype from "@/assets/logos/white/isotype.svg";
 import blackIsotype from "@/assets/logos/black/isotype.svg";
 
@@ -6,9 +8,16 @@ const { toggle, isDark } = useResolvedColorMode();
 </script>
 
 <template>
-  <header class="w-full h-auto my-5 p-5">
-    <nav
-      class="w-auto p-3 flex justify-between items-center border border-black/10 dark:border-white/10 rounded-full"
+  <header class="w-full mt-11 mb-[60px]">
+    <motion.nav
+      class="w-auto px-3 py-2 flex justify-between items-center border border-black/10 dark:border-white/10 rounded-full"
+      :initial="{ opacity: 0, scale: 0.5 }"
+      :animate="{ opacity: 1, scale: 1 }"
+      :transition="{
+        duration: 0.8,
+        delay: 0.5,
+        ease: [0, 0.71, 0.2, 1.01],
+      }"
     >
       <NuxtImg
         :src="!isDark ? blackIsotype : whiteIsotype"
@@ -17,18 +26,26 @@ const { toggle, isDark } = useResolvedColorMode();
         class="inline-block mr-2"
       />
 
-      <ul class="flex space-x-4">
-        <li><NuxtLink to="/">Home</NuxtLink></li>
-        <li><NuxtLink to="/lab">Lab</NuxtLink></li>
-      </ul>
+      <AppTabs
+        :tabs="[
+          { name: 'Home', to: '/' },
+          { name: 'Work', to: '/work' },
+          { name: 'About', to: '/about' },
+        ]"
+      />
 
       <button
         @click="toggle"
-        class="flex items-center justify-center size-8 ml-4 rounded-full bg-dark dark:bg-light"
+        class="flex items-center justify-center size-[30px] ml-4 rounded-full bg-dark dark:bg-light"
       >
-        <Icon name="ion:sunny-sharp" v-if="isDark" class="text-dark" />
-        <Icon name="ion:moon-sharp" v-else class="text-light" />
+        <Icon
+          size="14"
+          name="ion:sunny-sharp"
+          v-if="isDark"
+          class="text-dark"
+        />
+        <Icon size="14" name="ion:moon-sharp" v-else class="text-light" />
       </button>
-    </nav>
+    </motion.nav>
   </header>
 </template>
---
+++ frontend/app/components/App/Tabs.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { motion } from "motion-v";
+
+interface Tab {
+  name: string;
+  to: string;
+}
+
+const { tabs = [] } = defineProps<{
+  tabs?: Tab[];
+}>();
+
+const selectedTab = ref(0);
+
+const onTabChange = async (index: number) => {
+  selectedTab.value = index;
+  const tab = tabs[index];
+};
+</script>
+
+<template>
+  <nav>
+    <ul class="flex gap-1 align-center justify-center">
+      <li
+        v-for="(tab, index) in tabs"
+        :key="index"
+        :class="{ selected: selectedTab === index }"
+        role="tab"
+        :aria-selected="selectedTab === index"
+        class="relative"
+      >
+        <motion.div
+          v-if="selectedTab === index"
+          layout-id="selected-indicator"
+          class="absolute inset-0 rounded-full bg-dark/[6%] dark:bg-light/[4%]"
+        />
+
+        <motion.button
+          @press-start="onTabChange(index)"
+          :while-press="{ scale: 0.9 }"
+          :while-focus="{ backgroundColor: 'var(--accent-transparent)' }"
+          class="text-sm cursor-pointer bg-transparent px-3 py-1"
+        >
+          {{ tab.name }}
+        </motion.button>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped></style>
